281bf5016842fd0a3c4f3a1d9fdd75c7f00b42de
maheeen-khan <maheeenkhan4@gmail.com>
2024-07-30 15:28:39 +0500

    mode changer feature has updated

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-function changeMode(){
+function changeMode(event){
 
-    if(document.body.style.backgroundColor !== '#222222'){
+    if(document.body.style.backgroundColor === 'white'){
+
         document.body.style.backgroundColor = '#222222';
         document.getElementById('container-fluid').style.background = 'var(--dark-theme)';
         document.getElementById('dark_mode').innerText = `light_mode`;
+        console.log(event);
+
     }
     else{
-        console.log("hello");
+        
         document.body.style.backgroundColor = 'white';
         document.getElementById('container-fluid').style.background = 'var(--theme)';
         document.getElementById('dark_mode').innerText = `dark_mode`;
+        console.log(event);
+
     }
 
 }
