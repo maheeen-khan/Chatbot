9bc5c0184cd8d6e479feb2eec57092c689624b4b
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-01 01:55:22 +0500

    updated

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ function changeMode(event) {
 function enter(event){
     // var myMsg = document.getElementById('my-msg');
     // var chatMsg = document.getElementById('chatbot-msg');
-    if(event.keyCode === 13){
+    if(event.keyCode === 13 || event.type === 'click'){
 
         var myMsg = document.createElement('div');
         myMsg.className = 'ms-auto';
-        myMsg.style.width = "100px";
+        myMsg.style.width = "50%";
         myMsg.style.height = "100px";
         myMsg.style.background = "red";
         myMsg.style.color = "white";
@@ -46,7 +46,7 @@ function enter(event){
             userInput.value = "";
             // chatMsg.innerHTML = `hello how are you`;
             var chatMsg = document.createElement('div');
-            chatMsg.style.width = "100px";
+            chatMsg.style.width = "50%";
             chatMsg.style.height = "100px";
             chatMsg.style.background = "blue";
             chatMsg.style.color = "white";
@@ -60,7 +60,46 @@ function enter(event){
         },2000);
       
     }
+    
+    if((userInput.value).includes('fine') || (userInput.value).includes('good')){
 
+        setTimeout(function(){
+
+            userInput.value = "";
+            var chatMsg = document.createElement('div');
+            chatMsg.style.width = "50%";
+            chatMsg.style.height = "100px";
+            chatMsg.style.background = "blue";
+            chatMsg.style.color = "white";
+            chatMsg.style.borderRadius = '5px';
+            chatMsg.style.marginBottom = '5px';
+            chatMsg.innerHTML =  `I'm glad to hear that! Is there anything specific you would like to talk about or need help with today?`;
+
+        document.getElementById("my-msg").appendChild(chatMsg);
+
+
+        },2000);
+    }
+
+    if((userInput.value).includes('how') && (userInput.value).includes('you')){
+
+        setTimeout(function(){
+
+            userInput.value = "";
+            var chatMsg = document.createElement('div');
+            chatMsg.style.width = "50%";
+            chatMsg.style.height = "100px";
+            chatMsg.style.background = "blue";
+            chatMsg.style.color = "white";
+            chatMsg.style.borderRadius = '5px';
+            chatMsg.style.marginBottom = '5px';
+            chatMsg.innerHTML =  `I am Fine. What about you ?`;
+
+        document.getElementById("my-msg").appendChild(chatMsg);
+
+
+        },2000);
+    }
     // else{
     //     userInput.value = "";
     //     chatMsg.innerHTML = `Sorry I have not understand! Please elaborate further.`;
