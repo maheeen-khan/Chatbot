716778640db3b1b8093390fcfb95e68d84266dba
maheeen-khan <maheeenkhan4@gmail.com>
2024-07-31 18:15:22 +0500

    updated

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-function changeMode(event){
+function changeMode(event) {
 
-    if(document.body.style.backgroundColor === 'white'){
+    if (document.body.style.backgroundColor === 'white') {
 
         document.body.style.backgroundColor = '#222222';
         document.getElementById('container-fluid').style.background = 'var(--dark-theme)';
         document.getElementById('dark_mode').innerText = `light_mode`;
         document.getElementById('line').style.color = 'white';
+        document.getElementById('extra').style.backgroundColor = 'black';
     }
-    else{
-        
+    else {
+
         document.body.style.backgroundColor = 'white';
         document.getElementById('container-fluid').style.background = 'var(--theme)';
         document.getElementById('dark_mode').innerText = `dark_mode`;
         document.getElementById('line').style.color = 'black';
+        document.getElementById('extra').style.backgroundColor = 'white';
 
     }
 
+}
+
+function enter(event){
+    var myMsg = document.getElementById('my-msg');
+    var chatMsg = document.getElementById('chatbot-msg');
+    if(event.keyCode === 13){
+        myMsg.innerHTML = `${userInput.value}`;
+    }
+
+    if(['hi','hello','hey'].indexOf(userInput.value) !== -1){
+        // myMsg.innerHTML += `hello how are you`
+        chatMsg.innerHTML = `hello how are you`;
+        console.log('hello');
+    }
 }
